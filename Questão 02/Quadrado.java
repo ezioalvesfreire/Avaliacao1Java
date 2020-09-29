f6922cbd0b8e8aea195256f52af31f5e0727df51
Ézio Alves <ezio.freire@catolicasc.org.br>
2020-09-28 22:07:20 -0300

    primeiro commit

---
+++ Questão 02/Quadrado.java
@@ -0,0 +1,17 @@
+/* (a)
+Implemente uma classe `Quadrado`, que possui um atributo 
+`lado` de tipo `double`. Implemente um construtor que preenche 
+o atributo `lado`. Faça com que essa classe __implemente__ a 
+interface `Geometria`.
+*/
+public class Quadrado implements Geometria {
+    private double lado;
+    
+  public Quadrado(){
+     
+    }
+    public Quadrado(double l){
+       this.lado = lado;
+    }
+    
+ }
---
+++ Questão 02/QuadradoRedimensionavel.java
@@ -0,0 +1,33 @@
+/*
+Implemente uma classe `QuadradoRedimensionavel`, que é _subclasse_ de 
+`Quadrado` e implementa a interface `Redimensionavel`. 
+Essa classe deve _implementar_  um construtor que recebe o valor de 
+`lado` e o transmite para o construtor da __superclasse__.
+*/
+public class QuadradoRedimensionavel extends Quadrado implements Redimencionavel {
+    
+    public QuadradoRedimensionavel(double lado){
+        this.lado = lado;
+       
+      //  return lado;
+    
+    }
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+    //////////
+     @Override
+    public double getArea() {
+        return lado * lado;
+    }
+
+    @Override
+    public double getPerimetro() {
+        return 4 * lado;
+    }
+    
+}
